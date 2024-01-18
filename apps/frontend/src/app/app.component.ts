8956485f1d9d0ebf664db3f095604b48bc9b0c27
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-18 20:57:19 +0100

    refactor: minimize the angular component/directive selector prefix to `pet-market`

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   imports: [RouterModule],
-  selector: 'pet-marketplace-root',
+  selector: 'pet-market-root',
   template: `
     <p>Welcome Pet Market</p>
     <router-outlet></router-outlet>
