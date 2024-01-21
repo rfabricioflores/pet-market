4cfd05f529d812ef8856839ee81282c4a1a565ea
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-21 12:38:51 +0100

    feat: user entity

---
+++ apps/backend/src/main/java/se/fabricioflores/petmarket/model/User.java
@@ -0,0 +1,64 @@
+package se.fabricioflores.petmarket.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.AccessLevel;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import java.util.Objects;
+
+import org.hibernate.proxy.HibernateProxy;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  @Column(nullable = false, updatable = false, unique = true)
+  @Setter(AccessLevel.NONE)
+  private Long id;
+
+  @Column(unique = true, nullable = false)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String lastname;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  private String phone;
+
+
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null) return false;
+    Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+    Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) return false;
+    User user = (User) o;
+    return getId() != null && Objects.equals(getId(), user.getId());
+  }
+
+  @Override
+  public final int hashCode() {
+    return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+  }
+
+}
