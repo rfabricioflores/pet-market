456769e5beccb644340dca1d5c520db931439490
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-24 11:57:31 +0100

    APP-23 feat: basic ad entity

---
+++ apps/backend/src/main/java/se/fabricioflores/petmarket/model/Ad.java
@@ -0,0 +1,72 @@
+package se.fabricioflores.petmarket.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.proxy.HibernateProxy;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Ad {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  @Column(nullable = false, updatable = false, unique = true)
+  @Setter(AccessLevel.NONE)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  private String description;
+
+  @Column(nullable = false)
+  private BigDecimal price;
+
+  @UpdateTimestamp
+  @Column(nullable = false)
+  private LocalDateTime editedAt;
+
+  @CreationTimestamp
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User owner;
+
+
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null) return false;
+    Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+    Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) return false;
+    User user = (User) o;
+    return getId() != null && Objects.equals(getId(), user.getId());
+  }
+
+  @Override
+  public final int hashCode() {
+    return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+  }
+}
