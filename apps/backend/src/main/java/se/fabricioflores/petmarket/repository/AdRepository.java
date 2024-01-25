b2204fa1cd8f29412901d1b2705df22c6f2f3189
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-25 18:32:26 +0100

    APP-24 feat: ad repository with custom method declarations

--- apps/backend/src/main/java/se/fabricioflores/petmarket/repository/AdRepository.java
+++ apps/backend/src/main/java/se/fabricioflores/petmarket/repository/AdRepository.java
@@ -1,7 +1,13 @@
 package se.fabricioflores.petmarket.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import se.fabricioflores.petmarket.model.Ad;
+import se.fabricioflores.petmarket.projection.AdProjection;
+import se.fabricioflores.petmarket.projection.AdWithUserInfoProjection;
+import java.util.Optional;
 
-public interface AdRepository extends PagingAndSortingRepository<Ad, Long> {
+public interface AdRepository extends PagingAndSortingRepository<Ad, Long>, CrudRepository<Ad, Long> {
+  Optional<AdProjection> findOneById(Long id);
+  Optional<AdWithUserInfoProjection> findOneWithUserById(Long id);
 }
