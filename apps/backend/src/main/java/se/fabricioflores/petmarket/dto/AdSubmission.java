8db9652d789c1683e7344b2a45de9820090a901c
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-25 18:19:56 +0100

    APP-26 feat: static method for dto covertion to entity

--- apps/backend/src/main/java/se/fabricioflores/petmarket/dto/AdSubmission.java
+++ apps/backend/src/main/java/se/fabricioflores/petmarket/dto/AdSubmission.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
+import se.fabricioflores.petmarket.model.Ad;
+import se.fabricioflores.petmarket.model.User;
 
 public record AdSubmission(
   @NotNull @NotEmpty
@@ -14,5 +16,12 @@ public record AdSubmission(
   @NotNull @PositiveOrZero
   BigDecimal price
 ) {
-
+  public static Ad mappToAd(AdSubmission submission, User owner) {
+    Ad ad = new Ad();
+    ad.setTitle(submission.title());
+    ad.setDescription(submission.description());
+    ad.setPrice(submission.price());
+    ad.setOwner(owner);
+    return ad;
+  }
 }
