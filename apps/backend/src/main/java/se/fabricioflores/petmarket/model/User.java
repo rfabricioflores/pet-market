6b0aa96ba836fc9bedc43de591a9511145a01dae
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-25 18:34:37 +0100

    fix: fetch type eager for ads

--- apps/backend/src/main/java/se/fabricioflores/petmarket/model/User.java
+++ apps/backend/src/main/java/se/fabricioflores/petmarket/model/User.java
@@ -1,8 +1,8 @@
 package se.fabricioflores.petmarket.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
@@ -45,7 +45,7 @@ public class User {
 
   private String phone;
 
-  @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+  @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
   private Set<Ad> ads;
 
 
