789b193ff87b3dc80bd14362aa9c9208bfdf20b0
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-26 11:49:44 +0100

    feat: custom pageable list for ads

--- apps/backend/src/main/java/se/fabricioflores/petmarket/repository/AdRepository.java
+++ apps/backend/src/main/java/se/fabricioflores/petmarket/repository/AdRepository.java
@@ -1,5 +1,7 @@
 package se.fabricioflores.petmarket.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import se.fabricioflores.petmarket.model.Ad;
@@ -7,7 +9,8 @@ import se.fabricioflores.petmarket.projection.AdProjection;
 import se.fabricioflores.petmarket.projection.AdWithUserInfoProjection;
 import java.util.Optional;
 
-public interface AdRepository extends PagingAndSortingRepository<Ad, Long>, CrudRepository<Ad, Long> {
+public interface AdRepository extends PagingAndSortingRepository<Ad, Long>, CrudRepository<Ad, Long>{
   Optional<AdProjection> findOneById(Long id);
   Optional<AdWithUserInfoProjection> findOneWithUserById(Long id);
+  Page<AdProjection> findAllBy(Pageable pageable);
 }
