374710297ada27c785b82b0ebaed44394a7bae94
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-27 17:19:51 +0100

    chore: update projections

--- apps/backend/src/main/java/se/fabricioflores/petmarket/projection/AdProjection.java
+++ apps/backend/src/main/java/se/fabricioflores/petmarket/projection/AdProjection.java
@@ -2,12 +2,19 @@ package se.fabricioflores.petmarket.projection;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Set;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import se.fabricioflores.petmarket.model.AdPhoto;
 
+@JsonPropertyOrder({
+  "id", "title", "description", "price", "photos", "editedAt", "createdAt"
+})
 public interface AdProjection {
   Long getId();
   String getTitle();
   String getDescription();
   BigDecimal getPrice();
+  Set<AdPhoto> getPhotos();
   LocalDateTime getEditedAt();
   LocalDateTime getCreatedAt();
 }
