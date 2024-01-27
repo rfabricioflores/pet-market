ceacbe36e82d5ea56c7a683a51f2e3a28df42e33
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-27 17:18:51 +0100

    feat: photos entity

--- apps/backend/src/main/java/se/fabricioflores/petmarket/model/AdPhoto.java
+++ apps/backend/src/main/java/se/fabricioflores/petmarket/model/AdPhoto.java
@@ -1,34 +1,23 @@
 package se.fabricioflores.petmarket.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import static jakarta.persistence.GenerationType.IDENTITY;
-
-import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import static jakarta.persistence.GenerationType.IDENTITY;
 import java.util.Objects;
-import java.util.Set;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.proxy.HibernateProxy;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import lombok.AccessLevel;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Ad {
+public class AdPhoto {
 
   @Id
   @GeneratedValue(strategy = IDENTITY)
@@ -36,29 +25,12 @@ public class Ad {
   @Setter(AccessLevel.NONE)
   private Long id;
 
-  @Column(nullable = false)
-  private String title;
-
-  private String description;
-
-  @Column(nullable = false)
-  private BigDecimal price;
-
-  @OneToMany(mappedBy = "ad", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
-  private Set<AdPhoto> photos;
-
-  @UpdateTimestamp
-  @Column(nullable = false)
-  private LocalDateTime editedAt;
-
-  @CreationTimestamp
   @Column(nullable = false, updatable = false)
-  private LocalDateTime createdAt;
+  private String url;
 
   @JsonIgnore
   @ManyToOne
-  @JoinColumn(name = "user_id")
-  private User owner;
+  private Ad ad;
 
 
   @Override
@@ -68,12 +40,13 @@ public class Ad {
     Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
     Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
     if (thisEffectiveClass != oEffectiveClass) return false;
-    Ad ad = (Ad) o;
-    return getId() != null && Objects.equals(getId(), ad.getId());
+    AdPhoto adPhoto = (AdPhoto) o;
+    return getId() != null && Objects.equals(getId(), adPhoto.getId());
   }
 
   @Override
   public final int hashCode() {
     return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
   }
+
 }
