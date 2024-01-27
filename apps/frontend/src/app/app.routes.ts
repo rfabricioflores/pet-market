efb38802441adfad2b966068a87547b8b2bcfa62
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-27 22:57:23 +0100

    feat: lazy load home and adverts page components

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'adverts',
+    loadComponent: () =>
+      import('./pages/adverts/adverts.component').then(
+        (m) => m.AdvertsComponent
+      ),
+  },
+];
