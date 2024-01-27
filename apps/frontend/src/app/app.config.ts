33a3c8ee2f189ce669741a59cd0f168e99f71f37
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-27 20:26:23 +0100

    feat: provide http client and animations for the whole app

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -1,7 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withComponentInputBinding())],
+  providers: [
+    provideRouter(appRoutes, withComponentInputBinding()),
+    provideHttpClient(),
+    provideAnimations(),
+  ],
 };
