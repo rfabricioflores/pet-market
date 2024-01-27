c828f5760dee75adbbb5fa188bada5c549841e11
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-27 22:57:08 +0100

    feat: adverts page component

---
+++ apps/frontend/src/app/pages/adverts/adverts.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AdvertsComponent } from './adverts.component';
+
+describe('AdvertsComponent', () => {
+  let component: AdvertsComponent;
+  let fixture: ComponentFixture<AdvertsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AdvertsComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AdvertsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
