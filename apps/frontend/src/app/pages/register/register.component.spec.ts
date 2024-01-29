5f8f8b22a5b927862e56603075ff40d4a85c7c14
Fabricio Flores <32011023+rfabricioflores@users.noreply.github.com>
2024-01-29 09:17:35 +0100

    refactor: updated test

--- apps/frontend/src/app/pages/register/register.component.spec.ts
+++ apps/frontend/src/app/pages/register/register.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegisterComponent } from './register.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,7 +9,7 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterComponent, RouterTestingModule],
+      imports: [RegisterComponent, RouterTestingModule, HttpClientModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
