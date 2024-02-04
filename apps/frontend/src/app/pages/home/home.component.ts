a985b9cbeaf6f001fca98042445f5f6cc3c59d35
Fabricio <32011023+rfabricioflores@users.noreply.github.com>
2024-02-04 21:45:57 +0100

    refactor: declarative code

--- apps/frontend/src/app/pages/home/home.component.ts
+++ apps/frontend/src/app/pages/home/home.component.ts
@@ -1,17 +1,11 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, HostBinding, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from '../../components/common/search/search.component';
 import { AdPagination } from '../../interfaces/advertisement';
 import { AdService } from '../../services/ad/ad.service';
 import { AdCardListComponent } from '../../components/common/ad-card-list/ad-card-list.component';
 import { PaginatorModule } from 'primeng/paginator';
-
-interface PageEvent {
-  first: number;
-  rows: number;
-  page: number;
-  pageCount: number;
-}
+import { PaginatorState } from 'primeng/paginator/paginator.interface';
 
 @Component({
   selector: 'pet-market-home',
@@ -23,31 +17,40 @@ interface PageEvent {
     PaginatorModule,
   ],
   templateUrl: './home.component.html',
+  styles: `
+  :host {
+    display: block;
+  }
+  `,
 })
 export class HomeComponent implements OnInit {
+  @HostBinding('class')
+  readonly class = 'wrapper';
+
   private adService = inject(AdService);
 
   public adList = signal<AdPagination | null>(null);
 
+  public first = 0;
+  private page = 0;
+  public size = 10;
+
   ngOnInit() {
-    this.loadAds(0, 10);
+    this.loadAds();
   }
 
-  private loadAds(page: number, size: number) {
-    this.adService.getAdsWithPagination(page, size).subscribe({
+  private loadAds() {
+    this.adService.getAdsWithPagination(this.page, this.size).subscribe({
       next: (data) => this.adList.set(data),
       error: () => this.adList.set(null),
     });
   }
 
-  public first: number = 0;
-  public rows: number = 10;
+  public onPageChange(event: PaginatorState) {
+    event.first && (this.first = event.first);
+    event.rows && (this.size = event.rows);
+    event.page && (this.page = event.page);
 
-  onPageChange(event: unknown) {
-    const e = event as PageEvent;
-    this.first = e.first;
-    this.rows = e.rows;
-    console.log(e.page, this.rows);
-    this.loadAds(e.page, this.rows);
+    this.loadAds();
   }
 }
