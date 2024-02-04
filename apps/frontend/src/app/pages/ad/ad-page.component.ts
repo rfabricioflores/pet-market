d52cb316b7693dab22b5a494d55b2ea0401a0a68
Fabricio <32011023+rfabricioflores@users.noreply.github.com>
2024-02-04 21:42:47 +0100

    refactor: declarative code

--- apps/frontend/src/app/pages/ad/ad-page.component.ts
+++ apps/frontend/src/app/pages/ad/ad-page.component.ts
@@ -2,6 +2,8 @@ import {
   Component,
   HostBinding,
   OnInit,
+  Signal,
+  computed,
   inject,
   input,
   signal,
@@ -34,36 +36,19 @@ export class AdPageComponent implements OnInit {
   @HostBinding('class')
   readonly class = 'wrapper';
 
-  images: galleriImage[] = [];
-
   private adService = inject(AdService);
 
   private id = input<string>();
   public error = signal(false);
   public ad = signal<Ad | null>(null);
+  public adPhotos: Signal<unknown[] | undefined> = computed(() =>
+    this.ad()?.photos.map((item) => ({ itemImageSrc: item.url }))
+  );
 
   ngOnInit() {
     this.adService.getAdById(this.id()!).subscribe({
-      next: (ad) => {
-        this.ad.set(ad);
-
-        if (this.ad()?.photos.length !== 0) {
-          this.ad()?.photos.forEach(({ url }) => {
-            this.images.push({
-              itemImageSrc: url,
-            });
-          });
-        } else {
-          this.images.push({
-            itemImageSrc: '/assets/no-image.png',
-          });
-        }
-      },
+      next: (ad) => this.ad.set(ad),
       error: () => this.error.set(true),
     });
   }
-}
-
-interface galleriImage {
-  itemImageSrc: string;
 }
